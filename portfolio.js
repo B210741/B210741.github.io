09358a48e47ddae465e4225951ade80b6733bf06
Yen Culley <yen.nguyen.asia@gmail.com>
2023-06-09 19:52:49 +0100

    Upload revised Portfolio folder with added JavaScript

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,38 @@
-// Function submitForm is defined, which is triggered when the form is submitted.
-//It prevents the default form submission behavior using event.preventDefault()
-//and then displays an alert saying "Form submitted!". 
-function submitForm(event) {
-    event.preventDefault(); 
-    alert("Form submitted!");
+//Alert function and show message when the website is loaded
+window.onload = function() {
+    alert("Welcome to Yen Culley's Portfolio!");
+} 
+//Slideshow function
+let slideIndex = 0;
+showSlides();
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+  slideIndex++;
 }
